785ff8a91808ffe1ff88a2346a2f34fd95cea30e
shirayuki <shirayuki@46ki75.com>
2024-07-10 04:57:37 +0900

    fix: Add missing `has_more` key to Relation in Page object

--- src/page/properties/relation.rs
+++ src/page/properties/relation.rs
@@ -22,6 +22,11 @@ use serde::{Deserialize, Serialize};
 pub struct PageRelationProperty {
     pub id: String,
     pub relation: Vec<PageRelationPropertyParameter>,
+
+    /// If a relation has more than 25 references,
+    /// then the has_more value for the relation in the response object is true.
+    /// If a relation doesn’t exceed the limit, then has_more is false.
+    pub has_more: bool,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
