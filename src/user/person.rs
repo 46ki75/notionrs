e2275a3600932b3cc9f30e1191b1e9f9186a9d2d
shirayuki <shirayuki@46ki75.com>
2024-08-15 00:17:47 +0900

    test(Person): add unit test for deserialization of Person struct

--- src/user/person.rs
+++ src/user/person.rs
@@ -16,3 +16,34 @@ pub struct Person {
 pub struct PersonDetail {
     pub email: Option<String>,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json;
+
+    #[test]
+    fn unit_test_deserialize_person() {
+        let json_data = r#"
+        {
+            "object": "user",
+            "id": "78126152-3c2a-4b04-860e-77fb5bdded2f",
+            "name": "John Doe",
+            "avatar_url": "https://example.com/avatar.png",
+            "type": "person",
+            "person": {
+                "email": "johndoe@example.com"
+            }
+        }
+        "#;
+
+        let person: Person = serde_json::from_str(json_data).unwrap();
+
+        assert_eq!(person.object, "user");
+        assert_eq!(person.id, "78126152-3c2a-4b04-860e-77fb5bdded2f");
+        assert_eq!(person.name.unwrap(), "John Doe");
+        assert_eq!(person.avatar_url.unwrap(), "https://example.com/avatar.png");
+        assert_eq!(person.r#type.unwrap(), "person");
+        assert_eq!(person.person.unwrap().email.unwrap(), "johndoe@example.com");
+    }
+}
