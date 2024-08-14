cbb8e2af0cd0b7fbb5a430d6adc898d6742d614a
shirayuki <shirayuki@46ki75.com>
2024-08-15 04:14:55 +0900

    docs: create documentation for PageCheckboxProperty

--- src/page/properties/checkbox.rs
+++ src/page/properties/checkbox.rs
@@ -2,7 +2,15 @@ use serde::{Deserialize, Serialize};
 
 /// <https://developers.notion.com/reference/page-property-values#checkbox>
 ///
+/// - `$.['*'].id`: An underlying identifier for the property.
+///                 `id` remains constant when the property name changes.
+/// - `$.['*'].type`: Always `"button"`
+/// - `$.['*'].checkbox`: Whether the checkbox is checked (`true`) or unchecked (`false`).
+///
+/// **Note**: The `['*']` part represents the column name you set when creating the database.
+///
 /// Example checkbox page property value
+///
 /// ```json
 /// {
 ///     "Task completed": {
@@ -14,6 +22,10 @@ use serde::{Deserialize, Serialize};
 /// ```
 #[derive(Debug, Deserialize, Serialize)]
 pub struct PageCheckboxProperty {
+    /// An underlying identifier for the property.
+    /// `id` remains constant when the property name changes.
     pub id: String,
+
+    /// Whether the checkbox is checked (`true`) or unchecked (`false`).
     pub checkbox: bool,
 }
