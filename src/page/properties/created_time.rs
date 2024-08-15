5983c008c5ec499d05f8e2b007be674905dfbd53
shirayuki <shirayuki@46ki75.com>
2024-08-15 12:39:44 +0900

    test: add unit test for PageCreatedTimeProperty

--- src/page/properties/created_time.rs
+++ src/page/properties/created_time.rs
@@ -4,12 +4,13 @@ use serde::{Deserialize, Serialize};
 ///
 /// - `$.['*'].id`: An underlying identifier for the property.
 ///                 `id` remains constant when the property name changes.
-/// - `$.['*'].type`: Always `"__________"` // TODO: documentation replace placeholder
-/// - `$.['*'].__________`: // TODO: documentation
+/// - `$.['*'].type`: Always `"created_time"`
+/// - `$.['*'].created_time`: The date and time that the page was created.
+///                           The created_time value can’t be updated.
 ///
 /// **Note**: The `['*']` part represents the column name you set when creating the database.
 ///
-/// Example __________ page property value // TODO: documentation replace placeholder
+/// Example created_time page property value
 ///
 /// ```json
 /// {
@@ -26,7 +27,8 @@ pub struct PageCreatedTimeProperty {
     /// `id` remains constant when the property name changes.
     pub id: String,
 
-    // TODO: documentation
+    /// The date and time that the page was created.
+    ///The created_time value can’t be updated.
     pub created_time: String,
 }
 
@@ -38,5 +40,28 @@ pub struct PageCreatedTimeProperty {
 
 #[cfg(test)]
 mod tests {
-    // TODO: test
+    use super::*;
+
+    #[test]
+    fn unit_test_deserialize_page_created_time_property() {
+        let json_data = r#"
+        {
+            "Created time": {
+                "id": "sv%3Fi",
+                "type": "created_time",
+                "created_time": "2024-04-03T10:55:00.000Z"
+            }
+        }
+        "#;
+
+        let created_time_map = serde_json::from_str::<
+            std::collections::HashMap<String, PageCreatedTimeProperty>,
+        >(json_data)
+        .unwrap();
+
+        let created_time = created_time_map.get("Created time").unwrap();
+
+        assert_eq!(created_time.id, "sv%3Fi");
+        assert_eq!(created_time.created_time, "2024-04-03T10:55:00.000Z");
+    }
 }
