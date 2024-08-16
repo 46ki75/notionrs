08d22d27a7711391c0f524d9461014fc9484d486
shirayuki <shirayuki@46ki75.com>
2024-08-17 03:48:21 +0900

    docs: Fix documentation in get_self_extract_params.rs

--- examples/get_self_extract_params.rs
+++ examples/get_self_extract_params.rs
@@ -15,6 +15,8 @@ async fn main() -> Result<(), notionrs::error::NotionError> {
 
     Ok(())
 }
-// ```
+// Expected output:
+//
+// ```text
 // The integration name being used is `integration-name` (ID: b610aa5b-800e-4c2e-9d5c-72b72b5dedc0)
 // ```
