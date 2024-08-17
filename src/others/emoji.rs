2b4cf8c126bfa89c56568530c2634cd1ec8e5840
shirayuki <shirayuki@46ki75.com>
2024-08-18 03:50:08 +0900

    feat: implement Emoji builder

--- src/others/emoji.rs
+++ src/others/emoji.rs
@@ -6,3 +6,21 @@ pub struct Emoji {
     pub r#type: String,
     pub emoji: String,
 }
+
+impl Emoji {
+    pub fn new<T>(icon: T) -> Self
+    where
+        T: AsRef<str>,
+    {
+        Emoji {
+            r#type: "emoji".to_string(),
+            emoji: icon.as_ref().to_string(),
+        }
+    }
+}
+
+impl Default for Emoji {
+    fn default() -> Self {
+        Emoji::new("💡")
+    }
+}
