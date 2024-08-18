e8d05cfb774013b745857a6b8b695255d6725265
shirayuki <shirayuki@46ki75.com>
2024-08-18 18:56:18 +0000

    feat: create setter for TableRowBlock

--- src/block/table_row.rs
+++ src/block/table_row.rs
@@ -23,6 +23,11 @@ impl TableRowBlock {
     pub fn new() -> Self {
         Self::default()
     }
+
+    pub fn cells(mut self, rich_text: Vec<Vec<crate::others::rich_text::RichText>>) -> Self {
+        self.cells = rich_text;
+        self
+    }
 }
 
 // # --------------------------------------------------------------------------------
