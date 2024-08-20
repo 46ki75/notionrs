26887ca8e13d2d8d454e50dee90383e048b78423
shirayuki <shirayuki@46ki75.com>
2024-08-20 09:19:28 +0000

    docs: Add documentation for children in column and column_list

--- src/block/column.rs
+++ src/block/column.rs
@@ -2,6 +2,8 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug, Default)]
 pub struct ColumnBlock {
+    /// It can only be specified when making a block creation request.
+    /// If you need to retrieve the child blocks, you will have to send a request to this block again.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub children: Option<Vec<super::BlockType>>,
 }
--- src/block/column_list.rs
+++ src/block/column_list.rs
@@ -3,6 +3,8 @@ use serde::{Deserialize, Serialize};
 #[derive(Deserialize, Serialize, Debug, Default)]
 pub struct ColumnListBlock {
     /// Only `column` can be specified.
+    /// It can only be specified when making a block creation request.
+    /// If you need to retrieve the child blocks, you will have to send a request to this block again.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub children: Option<Vec<super::BlockType>>,
 }
