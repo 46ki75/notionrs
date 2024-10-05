ded7934f934e335420525aefd5b39894b333d56f
shirayuki <shirayuki@46ki75.com>
2024-10-05 17:54:11 +0900

    test(block): add integration test for Bulleted List Item block

---
+++ tests/block/crud_bulleted_list_item_block.rs
@@ -0,0 +1,87 @@
+mod integration_tests {
+
+    #[tokio::test]
+    async fn crud_bulleted_list_item_block() -> Result<(), notionrs::error::NotionError> {
+        dotenvy::dotenv().ok();
+
+        let block_id = std::env::var("NOTION_PAGE_ID").unwrap();
+
+        let client = notionrs::client::NotionClient::new();
+
+        // # --------------------------------------------------------------------------------
+        //
+        // append_block_children
+        //
+        // # --------------------------------------------------------------------------------
+
+        let rich_text = notionrs::rich_text!("list item");
+
+        let block = notionrs::block::Block::BulletedListItem {
+            bulleted_list_item: notionrs::block::BulletedListItemBlock::new()
+                .rich_text(vec![rich_text.clone()])
+                .blue_background(),
+        };
+
+        let request = client
+            .append_block_children()
+            .block_id(block_id.clone())
+            .children(vec![block]);
+
+        let response = request.send().await?;
+
+        // # --------------------------------------------------------------------------------
+        //
+        // get_block
+        //
+        // # --------------------------------------------------------------------------------
+
+        let request = client
+            .get_block()
+            .block_id(response.results.first().unwrap().id.clone());
+
+        let response = request.send().await?;
+
+        // # --------------------------------------------------------------------------------
+        //
+        // update_block
+        //
+        // # --------------------------------------------------------------------------------
+
+        let block = match response.block {
+            notionrs::block::Block::BulletedListItem { bulleted_list_item } => {
+                assert_eq!(bulleted_list_item.rich_text, vec![rich_text]);
+                assert_eq!(
+                    bulleted_list_item.color,
+                    notionrs::others::color::Color::blue_background()
+                );
+                notionrs::block::Block::BulletedListItem {
+                    bulleted_list_item: bulleted_list_item.green_background(),
+                }
+            }
+            e => panic!("{:?}", e),
+        };
+
+        let request = client
+            .update_block()
+            .block_id(response.id.clone())
+            .block(block);
+
+        let response = request.send().await?;
+
+        println!("{:?}", response);
+
+        // # --------------------------------------------------------------------------------
+        //
+        // delete_block
+        //
+        // # --------------------------------------------------------------------------------
+
+        let request = client.delete_block().block_id(response.id.clone());
+
+        let response = request.send().await?;
+
+        println!("{:?}", response);
+
+        Ok(())
+    }
+}
--- tests/block/mod.rs
+++ tests/block/mod.rs
@@ -1,5 +1,6 @@
 mod crud_audio_block;
 mod crud_bookmark_block;
 mod crud_breadcrumb_block;
+mod crud_bulleted_list_item_block;
 mod crud_heading_block;
 mod get_block_children;
