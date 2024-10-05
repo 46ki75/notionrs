7c2253c8b6619c529171639e8e3c2cd238b8e7db
shirayuki <shirayuki@46ki75.com>
2024-10-05 10:19:48 +0900

    test(block): add integration tests for Bookmark Block

---
+++ tests/block/crud_bookmark_block.rs
@@ -0,0 +1,78 @@
+mod integration_tests {
+
+    #[tokio::test]
+    async fn crud_bookmark_block() -> Result<(), notionrs::error::NotionError> {
+        dotenvy::dotenv().ok();
+
+        let block_id = std::env::var("NOTION_PAGE_ID").unwrap();
+
+        let client = notionrs::client::NotionClient::new();
+
+        // # --------------------------------------------------------------------------------
+        //
+        // append_block_children
+        //
+        // # --------------------------------------------------------------------------------
+
+        let request = client
+            .append_block_children()
+            .block_id(block_id.clone())
+            .children(vec![notionrs::block::Block::new_bookmark()
+                .url("https://example.com")
+                .caption(vec![notionrs::rich_text!("my caption")])
+                .build()]);
+
+        let response = request.send().await?;
+
+        // # --------------------------------------------------------------------------------
+        //
+        // get_block
+        //
+        // # --------------------------------------------------------------------------------
+
+        let request = client
+            .get_block()
+            .block_id(response.results.first().unwrap().id.clone());
+
+        let response = request.send().await?;
+
+        // # --------------------------------------------------------------------------------
+        //
+        // update_block
+        //
+        // # --------------------------------------------------------------------------------
+
+        let block = match response.details {
+            notionrs::block::Block::Bookmark { bookmark } => bookmark,
+            e => panic!("{:?}", e),
+        };
+
+        let builded_block = block
+            .url("https://example.com/index.html")
+            .caption(vec![])
+            .build();
+
+        let request = client
+            .update_block()
+            .block_id(response.id.clone())
+            .block(builded_block);
+
+        let response = request.send().await?;
+
+        println!("{:?}", response);
+
+        // # --------------------------------------------------------------------------------
+        //
+        // delete_block
+        //
+        // # --------------------------------------------------------------------------------
+
+        let request = client.delete_block().block_id(response.id.clone());
+
+        let response = request.send().await?;
+
+        println!("{:?}", response);
+
+        Ok(())
+    }
+}
--- tests/block/mod.rs
+++ tests/block/mod.rs
@@ -1,2 +1,3 @@
 mod crud_audio_block;
+mod crud_bookmark_block;
 mod get_block_children;
