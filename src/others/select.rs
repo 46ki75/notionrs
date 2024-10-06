c59eb89d5fa40cf897a350f4b8158e09b04e6845
shirayuki <shirayuki@46ki75.com>
2024-10-06 22:17:07 +0900

    docs(SelectColor): clarify differences from regular Color

--- src/others/select.rs
+++ src/others/select.rs
@@ -16,6 +16,12 @@ pub struct SelectGroup {
     pub option_ids: Vec<String>,
 }
 
+/// Specifies the label colors. These colors can be used in the following property types.
+/// This color enumeration is different from the usual Color (which includes Background Color).
+///
+/// - [Status (docs)](https://developers.notion.com/reference/property-object#status)
+/// - [Select (docs)](https://developers.notion.com/reference/property-object#select)
+/// - [Multi-select (docs)](https://developers.notion.com/reference/property-object#multi-select)
 #[derive(Debug, Deserialize, Serialize, Clone, Copy, PartialEq, Eq, Default)]
 #[serde(rename_all = "snake_case")]
 pub enum SelectColor {
