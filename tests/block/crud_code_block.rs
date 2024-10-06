a4b11aec6b74ab62a30cab579518f9a2f65edba2
shirayuki <shirayuki@46ki75.com>
2024-10-06 22:55:55 +0900

    test(block): add integration tests for code block

---
+++ tests/block/crud_code_block.rs
@@ -0,0 +1,87 @@
+mod integration_tests {
+
+    #[tokio::test]
+    async fn crud_code_block() -> Result<(), notionrs::error::Error> {
+        dotenvy::dotenv().ok();
+
+        let block_id = std::env::var("NOTION_PAGE_ID").unwrap();
+
+        let client = notionrs::client::NotionClient::new();
+
+        // # --------------------------------------------------------------------------------
+        //
+        // append_block_children
+        //
+        // # --------------------------------------------------------------------------------
+
+        let rich_text = notionrs::RichText::from("console.log(0)");
+
+        let block = notionrs::block::Block::Code {
+            code: notionrs::block::CodeBlock::new()
+                .rich_text(vec![rich_text.clone()])
+                .lnaguage(notionrs::others::language::Language::Javascript),
+        };
+
+        let request = client
+            .append_block_children()
+            .block_id(block_id.clone())
+            .children(vec![block]);
+
+        let response = request.send().await?;
+
+        // # --------------------------------------------------------------------------------
+        //
+        // get_block
+        //
+        // # --------------------------------------------------------------------------------
+
+        let request = client
+            .get_block()
+            .block_id(response.results.first().unwrap().id.clone());
+
+        let response = request.send().await?;
+
+        // # --------------------------------------------------------------------------------
+        //
+        // update_block
+        //
+        // # --------------------------------------------------------------------------------
+
+        let block = match response.block {
+            notionrs::block::Block::Code { code } => {
+                assert_eq!(code.rich_text, vec![rich_text]);
+                assert_eq!(
+                    code.language,
+                    notionrs::others::language::Language::Javascript
+                );
+                notionrs::block::Block::Code {
+                    code: code.lnaguage(notionrs::others::language::Language::Typescript),
+                }
+            }
+            e => panic!("{:?}", e),
+        };
+
+        let request = client
+            .update_block()
+            .block_id(response.id.clone())
+            .block(block);
+
+        let response = request.send().await?;
+
+        println!("{:?}", response);
+
+        // # --------------------------------------------------------------------------------
+        //
+        // delete_block
+        //
+        // # --------------------------------------------------------------------------------
+
+        let request = client.delete_block().block_id(response.id.clone());
+
+        let response = request.send().await?;
+
+        println!("{:?}", response);
+
+        Ok(())
+    }
+}
