3e32c6684b88cec1341bd1723e462db4c672ed5a
shirayuki <shirayuki@46ki75.com>
2024-10-06 22:55:55 +0900

    test(block): add integration tests for code block

--- tests/block/mod.rs
+++ tests/block/mod.rs
@@ -3,5 +3,6 @@ mod crud_bookmark_block;
 mod crud_breadcrumb_block;
 mod crud_bulleted_list_item_block;
 mod crud_callout_block;
+mod crud_code_block;
 mod crud_heading_block;
 mod get_block_children;
