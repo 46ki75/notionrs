a19dbdbca1826a66821ef6b6aa0bb9f7842c1e4e
shirayuki <shirayuki@46ki75.com>
2024-10-06 23:54:07 +0900

    feat(block): add integration test for embed block

--- tests/block/mod.rs
+++ tests/block/mod.rs
@@ -5,5 +5,6 @@ mod crud_bulleted_list_item_block;
 mod crud_callout_block;
 mod crud_code_block;
 mod crud_column_list_block;
+mod crud_embed_block;
 mod crud_heading_block;
 mod get_block_children;
