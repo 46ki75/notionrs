228e83e93fb8db0970cd0fd125ab61534a6a4bac
shirayuki <shirayuki@46ki75.com>
2024-10-26 13:28:40 +0900

    docs(database): add descriptions for fields

--- src/database/properties/phone_number.rs
+++ src/database/properties/phone_number.rs
@@ -2,15 +2,20 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug, Default, Clone, PartialEq, Eq)]
 pub struct DatabasePhoneNumberProperty {
+    /// Property Identifier
     #[serde(skip_serializing)]
     pub id: Option<String>,
 
+    /// Modify the value of this field when updating the column name of the property.
     #[serde(skip_serializing)]
     pub name: String,
 
+    /// Although it is not explicitly stated in the official documentation,
+    /// you can add a description to the property by specifying this.
     #[serde(skip_serializing)]
     pub description: Option<String>,
 
+    /// An empty object (`{}`)
     pub phone_number: std::collections::HashMap<(), ()>,
 }
 
--- src/database/properties/title.rs
+++ src/database/properties/title.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug, Default, Clone, PartialEq, Eq)]
 pub struct DatabaseTitleProperty {
+    /// Property Identifier
     #[serde(skip_serializing)]
     pub id: Option<String>,
 
@@ -9,9 +10,12 @@ pub struct DatabaseTitleProperty {
     #[serde(skip_serializing)]
     pub name: String,
 
+    /// Although it is not explicitly stated in the official documentation,
+    /// you can add a description to the property by specifying this.
     #[serde(skip_serializing)]
     pub description: Option<String>,
 
+    /// An empty object (`{}`)
     pub title: std::collections::HashMap<(), ()>,
 }
 
