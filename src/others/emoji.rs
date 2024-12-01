1bcd7d6b536f1b6831fbaef45b96555a05b651bc
shirayuki <shirayuki@46ki75.com>
2024-12-01 13:58:05 +0900

    feat: enhance Emoji struct with Display implementation and add unit tests

--- src/others/emoji.rs
+++ src/others/emoji.rs
@@ -1,9 +1,12 @@
 use serde::{Deserialize, Serialize};
 
 /// <https://developers.notion.com/reference/emoji-object>
-#[derive(Debug, Deserialize, Serialize, Clone)]
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Eq)]
 pub struct Emoji {
+    /// The constant string "emoji" that represents the object type.
     pub r#type: String,
+
+    /// The emoji character. For example, "😻".
     pub emoji: char,
 }
 
@@ -26,5 +29,35 @@ impl Default for Emoji {
 impl From<char> for Emoji {
     fn from(value: char) -> Self {
         Self::default().emoji(value)
+    }
+}
+
+impl std::fmt::Display for Emoji {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.emoji)
+    }
+}
+
+// # --------------------------------------------------------------------------------
+//
+// unit test
+//
+// # --------------------------------------------------------------------------------
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_emoji() {
+        let emoji = Emoji::default();
+        assert_eq!(emoji.r#type, "emoji");
+        assert_eq!(emoji.emoji, '💡');
+        assert_eq!(emoji.to_string(), "💡");
+
+        let emoji = Emoji::from('🔥');
+        assert_eq!(emoji.r#type, "emoji");
+        assert_eq!(emoji.emoji, '🔥');
+        assert_eq!(emoji.to_string(), "🔥");
     }
 }
