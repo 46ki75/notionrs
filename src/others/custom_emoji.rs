6ee58a542e6ac7d323273d18ca663d1af14c7ccc
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-02-22 01:22:07 +0000

    feat: enhance Mention enum with CustomEmoji variant and update display logic

--- src/others/custom_emoji.rs
+++ src/others/custom_emoji.rs
@@ -3,7 +3,7 @@ use core::fmt;
 /// <https://developers.notion.com/reference/emoji-object#custom-emoji>
 #[derive(Debug, serde::Deserialize, serde::Serialize, Clone, PartialEq, Eq)]
 pub struct CustomEmoji {
-    /// The constant string "emoji" that represents the object type.
+    /// The constant string "custom_emoji" that represents the object type.
     pub r#type: String,
 
     /// Custom emoji object, containing id, name, url
@@ -20,5 +20,11 @@ pub struct CustomEmojiContent {
 impl fmt::Display for CustomEmoji {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.custom_emoji.name)
+    }
+}
+
+impl fmt::Display for CustomEmojiContent {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.name)
     }
 }
