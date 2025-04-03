45e8edc210a9d020479e4b166c1c8444b1e614d3
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-04-03 17:49:34 +0000

    feat: add new GitHub issue labels with descriptions for better issue tracking

--- terraform/github/label.tf
+++ terraform/github/label.tf
@@ -1,7 +1,29 @@
 resource "github_issue_label" "bug" {
-  repository = github_repository.notionrs.name
-  name       = "bug"
-  color      = "8e3636"
+  repository  = github_repository.notionrs.name
+  name        = "Bug"
+  color       = "8e3636"
+  description = "Something isn't working."
+}
+
+resource "github_issue_label" "duplicate" {
+  repository  = github_repository.notionrs.name
+  name        = "duplicate"
+  color       = "bec2ca"
+  description = "This issue or pull request already exists."
+}
+
+resource "github_issue_label" "enhancement" {
+  repository  = github_repository.notionrs.name
+  name        = "enhancement"
+  color       = "aebed9"
+  description = "New feature or request."
+}
+
+resource "github_issue_label" "good_first_issue" {
+  repository  = github_repository.notionrs.name
+  name        = "good first issue"
+  color       = "725cf5"
+  description = "Good for newcomers"
 }
 
 resource "github_issue_label" "dependencies" {
@@ -39,9 +61,23 @@ resource "github_issue_label" "rust" {
   description = "Pull requests that update Cargo.toml or its lock file."
 }
 
-resource "github_issue_label" "good_first_issue" {
+resource "github_issue_label" "main_to_develop" {
   repository  = github_repository.notionrs.name
-  name        = "good first issue"
-  color       = "725cf5"
-  description = "Good for newcomers"
+  name        = "main → develop"
+  color       = "34744c"
+  description = "Synchronize changes from the main branch into develop."
+}
+
+resource "github_issue_label" "release" {
+  repository  = github_repository.notionrs.name
+  name        = "Release"
+  color       = "3c557f"
+  description = "Merge release changes into the main branch."
+}
+
+resource "github_issue_label" "version_bump" {
+  repository  = github_repository.notionrs.name
+  name        = "Version Bump"
+  color       = "555b67"
+  description = "Update version numbers for release."
 }
