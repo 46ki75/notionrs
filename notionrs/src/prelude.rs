ed0a51114747208dba29dd2e047621788d5e1726
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-05-04 11:13:32 +0000

    fix: remove redundant wildcard import in page module

--- notionrs/src/prelude.rs
+++ notionrs/src/prelude.rs
@@ -15,7 +15,7 @@ pub use crate::object::{
         PageProperty, PageResponse, button::*, checkbox::*, created_by::*, created_time::*,
         date::*, email::*, files::*, formula::*, last_edited_by::*, last_edited_time::*,
         multi_select::*, number::*, people::*, phone_number::*, relation::*, rich_text::*,
-        rollup::*, select::*, status::*, title::*, unique_id::*, url::*, verification::*, *,
+        rollup::*, select::*, status::*, title::*, unique_id::*, url::*, verification::*,
     },
     request::{filter::*, search::*, sort::*},
     rich_text::{RichText, RichTextAnnotations, equation::*, mention::*, text::*},
