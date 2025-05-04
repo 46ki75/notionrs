933f4ab18a22620b9828c63be7990de23832a907
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-05-04 01:48:19 +0000

    feat: enhance prelude by adding Block and Page properties for improved structure

--- notionrs/src/prelude.rs
+++ notionrs/src/prelude.rs
@@ -1,24 +1,24 @@
 pub use crate::object::{
     block::{
-        bookmark::*, bulleted_list_item::*, callout::*, child_database::*, child_page::*, code::*,
-        column::*, column_list::*, embed::*, equation::*, heading::*, link_preview::*,
-        numbered_list_item::*, paragraph::*, quote::*, synced_block::*, table::*,
+        Block, BlockResponse, bookmark::*, bulleted_list_item::*, callout::*, child_database::*,
+        child_page::*, code::*, column::*, column_list::*, embed::*, equation::*, heading::*,
+        link_preview::*, numbered_list_item::*, paragraph::*, quote::*, synced_block::*, table::*,
         table_of_contents::*, table_row::*, template::*, to_do::*, toggle::*,
     },
     database::{
-        button::*, checkbox::*, created_by::*, created_time::*, date::*, email::*, files::*,
-        formula::*, last_edited_by::*, last_edited_time::*, multi_select::*, number::*, people::*,
-        phone_number::*, relation::*, rich_text::*, rollup::*, select::*, status::*, title::*,
-        unique_id::*, url::*, verification::*,
+        DatabaseProperty, DatabaseResponse, button::*, checkbox::*, created_by::*, created_time::*,
+        date::*, email::*, files::*, formula::*, last_edited_by::*, last_edited_time::*,
+        multi_select::*, number::*, people::*, phone_number::*, relation::*, rich_text::*,
+        rollup::*, select::*, status::*, title::*, unique_id::*, url::*, verification::*,
     },
     page::{
-        button::*, checkbox::*, created_by::*, created_time::*, date::*, email::*, files::*,
-        formula::*, last_edited_by::*, last_edited_time::*, multi_select::*, number::*, people::*,
-        phone_number::*, relation::*, rich_text::*, rollup::*, select::*, status::*, title::*,
-        unique_id::*, url::*, verification::*,
+        PageProperty, PageResponse, button::*, checkbox::*, created_by::*, created_time::*,
+        date::*, email::*, files::*, formula::*, last_edited_by::*, last_edited_time::*,
+        multi_select::*, number::*, people::*, phone_number::*, relation::*, rich_text::*,
+        rollup::*, select::*, status::*, title::*, unique_id::*, url::*, verification::*, *,
     },
     request::{filter::*, search::*, sort::*},
-    rich_text::{equation::*, mention::*, text::*},
+    rich_text::{RichText, RichTextAnnotations, equation::*, mention::*, text::*},
 };
 
 pub use crate::object::{
