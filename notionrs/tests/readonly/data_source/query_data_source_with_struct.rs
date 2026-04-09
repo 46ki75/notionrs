1c5cf233a3e6fd52e66d71a530711b9f3fd15c9f
Ikuma Yamashita <me@ikuma.cloud>
2026-04-09 19:44:56 +0900

    test: expand MyCrateProperties struct with additional fields for integration tests

--- notionrs/tests/readonly/data_source/query_data_source_with_struct.rs
+++ notionrs/tests/readonly/data_source/query_data_source_with_struct.rs
@@ -14,8 +14,68 @@ mod integration_tests {
 
     #[derive(Debug, Clone, Serialize, Deserialize)]
     struct MyCrateProperties {
+        #[serde(rename = "ID")]
+        pub id: PageUniqueIdProperty,
+
         #[serde(rename = "My Title")]
         pub my_title: PageTitleProperty,
+
+        #[serde(rename = "Text")]
+        pub text: PageRichTextProperty,
+
+        #[serde(rename = "URL")]
+        pub url: PageUrlProperty,
+
+        #[serde(rename = "API Type")]
+        pub api_type: PageSelectProperty,
+
+        #[serde(rename = "Status")]
+        pub status: PageStatusProperty,
+
+        #[serde(rename = "Multi-select")]
+        pub multi_select: PageMultiSelectProperty,
+
+        #[serde(rename = "Checkbox")]
+        pub checkbox: PageCheckboxProperty,
+
+        #[serde(rename = "My Date")]
+        pub my_date: PageDateProperty,
+
+        #[serde(rename = "User")]
+        pub user: PagePeopleProperty,
+
+        #[serde(rename = "Phone Number")]
+        pub phone_number: PagePhoneNumberProperty,
+
+        #[serde(rename = "Files & media")]
+        pub files_and_media: PageFilesProperty,
+
+        #[serde(rename = "Number")]
+        pub number: PageNumberProperty,
+
+        #[serde(rename = "Related Read-only: Integration Test")]
+        pub related_read_only_integration_test: PageRelationProperty,
+
+        #[serde(rename = "Related back to Read-only: Integration Test")]
+        pub related_back_to_read_only_integration_test: PageRelationProperty,
+
+        #[serde(rename = "Rollup")]
+        pub rollup: PageRollupProperty,
+
+        #[serde(rename = "Place")]
+        pub place: PagePlaceProperty,
+
+        #[serde(rename = "Created time")]
+        pub created_time: PageCreatedTimeProperty,
+
+        #[serde(rename = "Updated time")]
+        pub updated_time: PageLastEditedTimeProperty,
+
+        #[serde(rename = "Created by")]
+        pub updated_by: PageCreatedByProperty,
+
+        #[serde(rename = "Last edited by")]
+        pub last_edited_by: PageLastEditedByProperty,
     }
 
     #[tokio::test]
