b361561a1bf54a2a364be2d5f2d7809822fff90d
Ikuma Yamashita <me@ikuma.cloud>
2026-04-11 02:04:14 +0900

    feat: add view_query module with integration tests for view queries

--- notionrs/tests/readonly/view/mod.rs
+++ notionrs/tests/readonly/view/mod.rs
@@ -1,2 +1,3 @@
 mod list_views;
 mod retrieve_view;
+mod view_query;
---
+++ notionrs/tests/readonly/view/view_query.rs
@@ -0,0 +1,53 @@
+// `create_view_query` + `get_view_query_results` + `delete_view_query`
+
+mod integration_tests {
+
+    /// <https://www.notion.so/33ea03d79b2680c9a869fd22af127b56?v=33ea03d79b268028873a000c099dd218>
+    static VIEW_ID: &str = "33ea03d79b268028873a000c099dd218";
+
+    #[tokio::test]
+    async fn view_query() {
+        dotenvy::dotenv().ok();
+
+        let notion_api_key = std::env::var("NOTION_API_KEY_READONLY").unwrap();
+
+        let client = notionrs::Client::new(notion_api_key);
+
+        // `create_view_query`
+
+        let results = client
+            .create_view_query()
+            .view_id(VIEW_ID)
+            .send()
+            .await
+            .unwrap();
+
+        println!("{}", serde_json::to_string(&results).unwrap());
+
+        // `get_view_query_results`
+
+        let query_id = results.id;
+
+        let results = client
+            .get_view_query_results()
+            .view_id(VIEW_ID)
+            .query_id(&query_id)
+            .send()
+            .await
+            .unwrap();
+
+        println!("{}", serde_json::to_string(&results).unwrap());
+
+        // `delete_view_query`
+
+        let results = client
+            .delete_view_query()
+            .view_id(VIEW_ID)
+            .query_id(&query_id)
+            .send()
+            .await
+            .unwrap();
+
+        println!("{}", serde_json::to_string(&results).unwrap());
+    }
+}
